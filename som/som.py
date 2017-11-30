ac203c4458496afe1e7e12749284f690311f8b1e
yinzhibo <1350526138@qq.com>
2017-11-30 19:59:29 +0800

    注释文档

--- som/som.py
+++ som/som.py
@@ -10,22 +10,25 @@ import numpy as np
 from numpy.linalg import cholesky
 import math
 
-   
+#初始化输入输出层链接权值  
 def initCompetition(n,m,d):
     array = np.random.random(size = n*m*d)
     com_weight = array.reshape(n,m,d)
     return com_weight
+#计算欧氏距离
 def cal2NF(data):
     res = 0
     for x in data:
         res = res + x*x
     return res**0.5
+#将数据归一化
 def normalize_data(train_data):
     for data in train_data:
         two_NF = cal2NF(data)
         for i in range(len(data)):
             data[i] = data[i] / two_NF
     return train_data
+#将权值归一化
 def nomalize_weight(com_weight):
     for x in com_weight:
         for data in x:
@@ -33,6 +36,7 @@ def nomalize_weight(com_weight):
             for i in range(len(data)):
                 data[i] = data[i] / two_NF
     return com_weight
+#得到获胜神经元索引
 def getWinner(data,com_weight):
     max_sim = 0
     mark_n = 0
@@ -45,6 +49,7 @@ def getWinner(data,com_weight):
                 mark_n = i
                 mark_m = j
     return mark_n,mark_m
+#得到获胜神经元周围的兴奋神经元的索引
 def getNeighbor(n,m,N_neighbor,com_weight):
     res = []
     nn,mm,cc = np.shape(com_weight)
@@ -54,8 +59,11 @@ def getNeighbor(n,m,N_neighbor,com_weight):
             if N <= N_neighbor:
                 res.append((i,j,N))
     return res
+#学习率 与迭代次数和拓扑距离相关
 def eta(t,N):
-    return (0.3/(t+1))*(math.e**(-N))                      
+    return (0.3/(t+1))*(math.e**(-N))    
+
+#som算法核心                  
 def som(train_data,com_weight,T,N_neighbor):
     for t in range(T-1):
         com_weight = nomalize_weight(com_weight)
@@ -80,7 +88,9 @@ def som(train_data,com_weight,T,N_neighbor):
             kind = kind + 1
             res[key] = []
             res[key].append(i)
+#手动生成数据
             
+
 #设置高斯分布的sigma
 sigma1=np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
 sigma2=np.array([[0.81,0,0,0],[0,0.81,0,0],[0,0,0.81,0],[0,0,0,0.81]])
